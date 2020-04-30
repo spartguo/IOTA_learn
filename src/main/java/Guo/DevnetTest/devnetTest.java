02b745066dccb59c2dc5cd2843a1a35142d69188
GuoYQ <172544714@qq.com>
2020-04-30 18:05:51 +0800

    添加根据深度找tip的方法

--- src/main/java/Guo/DevnetTest/devnetTest.java
+++ src/main/java/Guo/DevnetTest/devnetTest.java
@@ -2,6 +2,8 @@ package Guo.DevnetTest;
 
 import Guo.Common.generateTestAddress;
 import Guo.Common.testTransferTime;
+import org.iota.jota.IotaAPI;
+import org.iota.jota.dto.response.GetTransactionsToApproveResponse;
 import org.iota.jota.model.Input;
 
 import java.util.ArrayList;
@@ -10,21 +12,34 @@ import java.util.List;
 public class devnetTest {
     public static void main(String []args){
 //        for (int i = 0;i < 10;i++)
-            testMainTime(100);
+//            testMainTime(100);
+        testTipSelect(1);
     }
 
-    public static void testTipSelect(){
+
+    /**
+     * 通过getTransactiontoApprove进行tipselection，可以控制深度
+     */
+    public static void testTipSelect(int dept){
         String protocol = "https";
         String node = "nodes.iota.cafe";
         int port = 443;
 
-        testTransferTime.testTimeByTipSelection(protocol,node,port);
+//        testTransferTime.testTimeByTipSelection(protocol,node,port);
+        IotaAPI api = testTransferTime.getApi(protocol,node,port);
+        int tmp = 0;
+        for (int i = 0;i < 10 ; i++) {
+            GetTransactionsToApproveResponse txs = api.getTransactionsToApprove(dept, null);
+            System.out.println("duration " + txs.getDuration() + " ms");
+            tmp += txs.getDuration();
+        }
+        System.out.println(" dept = " + dept +"\n avg = " + tmp/10 + " ms");
     }
 
 
 
     /**
-     * 主测试方法
+     * 主测试方法，通过seed给其他的seed地址发送交易
      */
     public static void testMainTime(int count){
         String protocol = "https";
